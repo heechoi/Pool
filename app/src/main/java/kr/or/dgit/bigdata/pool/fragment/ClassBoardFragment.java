68f353a3d2ebb8dde96bee92fbbdbed71a6aed7e
HongBumHa <goldstate@naver.com>
2018-04-17 17:31:20 +0900

    다이얼로그 띄우기

--- app/src/main/java/kr/or/dgit/bigdata/pool/fragment/ClassBoardFragment.java
+++ app/src/main/java/kr/or/dgit/bigdata/pool/fragment/ClassBoardFragment.java
@@ -1,12 +1,15 @@
 package kr.or.dgit.bigdata.pool.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -14,7 +17,7 @@ import kr.or.dgit.bigdata.pool.MainActivity;
 import kr.or.dgit.bigdata.pool.R;
 
 public class ClassBoardFragment extends Fragment {
-    ArrayAdapter<CharSequence> adspin;
+
     public static ClassBoardFragment newInstance(){
         ClassBoardFragment cf = new ClassBoardFragment();
         return cf;
@@ -25,22 +28,25 @@ public class ClassBoardFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.classboard, container, false);
-        Spinner spinner = (Spinner)root.findViewById(R.id.cls_board_spinner);
-        adspin = ArrayAdapter.createFromResource(getActivity(),R.array.classboard_selected,android.R.layout.simple_spinner_item);
-        adspin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adspin);
-        spinner.setPrompt("반을 선택하세요");
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        Button cls_board_btn = (Button)root.findViewById(R.id.cls_board_btn);
+        cls_board_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getActivity(),adspin.getItem(i) + "을 선택 했습니다.", Toast.LENGTH_LONG).show();
-            }
+            public void onClick(View view) {
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
+                new AlertDialog.Builder(getActivity())
+                        .setIcon(R.mipmap.ic_launcher)
+                        .setTitle("반을 선택하세요")
+                        .setItems(R.array.classboard_selected, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                String[] arrays = getResources().getStringArray(R.array.classboard_selected);
+                                Toast.makeText(getActivity(), arrays[i], Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("취소",null).create().show();
             }
         });
+
         return root;
     }
 }
