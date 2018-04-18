f711becef296048603b42077b2d0672542167e0f
KangHyun Lee <rkd7327@naver.com>
2018-04-18 16:00:42 +0900

    강사모드 - 회원리스트보기

--- app/src/main/java/kr/or/dgit/bigdata/pool/fragment/ClassInfoFragment.java
+++ app/src/main/java/kr/or/dgit/bigdata/pool/fragment/ClassInfoFragment.java
@@ -1,6 +1,7 @@
 package kr.or.dgit.bigdata.pool.fragment;
 
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,8 +12,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TabHost;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -21,6 +27,7 @@ import org.json.JSONObject;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 import kr.or.dgit.bigdata.pool.R;
 import kr.or.dgit.bigdata.pool.dto.Member;
@@ -33,6 +40,7 @@ public class ClassInfoFragment extends Fragment implements View.OnClickListener{
     String[] arrays;
     int[] cno;
     int tno;
+    ListView listView;
 
     public static ClassInfoFragment newInstance(){
         ClassInfoFragment cf = new ClassInfoFragment();
@@ -72,7 +80,7 @@ public class ClassInfoFragment extends Fragment implements View.OnClickListener{
         host.addTab(spec);
         tno = 0;
         new HttpRequestTack(getContext(),mHandler,"GET","정보를 가져오고 있습니다...").execute(http+"/restclassinfo/classlist?tno=1004");
-
+        listView = root.findViewById(R.id.member_list);
         return root;
     }
 
@@ -85,7 +93,7 @@ public class ClassInfoFragment extends Fragment implements View.OnClickListener{
                     .setItems(arrays, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i){
-                            Toast.makeText(getActivity(), cno[i], Toast.LENGTH_SHORT).show();
+
                             new HttpRequestTack(getContext(),mHandler,"GET","정보를 가져오고 있습니다...",2).execute(http+"/restregister/memberList?cno="+cno[i]);
                         }
                     })
@@ -133,17 +141,90 @@ public class ClassInfoFragment extends Fragment implements View.OnClickListener{
                             member.setAge((int)order.get("age"));
                             member.setTell((String)order.get("tell"));
                             mList.add(member);
-
                         }
                     } catch (JSONException e) {
                         Log.i("Json_parser", e.getMessage());
                     }
+                    MyListAdapter adapter = new MyListAdapter(getContext(),R.layout.member_item,mList);
+                    listView.setAdapter(adapter);
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                    Toast.makeText(getActivity(), mList.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+
                 }
                 break;
             }
         }
     };
+
+
+    class MyListAdapter extends BaseAdapter {
+        private static final String TAG = "MyListAdapter";
+        private Context mContext;  //MyListAdapter 가 Activity가 아니므로
+        private int mItemRowLayout; //항목레이아웃
+        private LayoutInflater mInflater; //항목레이아웃을 전개할 전개자
+        private List<Member> arItem;
+
+        public MyListAdapter(Context context, int itemRowLayout, List<Member> list) {
+            mContext = context;
+            mItemRowLayout = itemRowLayout;
+            arItem = list;
+            mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
+        }
+
+        @Override
+        public int getCount() {
+            Log.d(TAG,"getCount()");
+            return arItem.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            Log.d(TAG,"getItem()");
+            return arItem.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            Log.d(TAG,"getItemId()");
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertview, ViewGroup viewGroup) {
+            Log.d(TAG,"getView()");
+            if(convertview == null){
+                convertview = mInflater.inflate(mItemRowLayout,viewGroup,false);
+            }
+
+            TextView name = convertview.findViewById(R.id.name_text);
+            name.setText(arItem.get(position).getName());
+
+            TextView gender = convertview.findViewById(R.id.gender_text);
+            gender.setText(arItem.get(position).getGender());
+
+            TextView age  = convertview.findViewById(R.id.age_text);
+            String ageSub = String.valueOf(arItem.get(position).getAge());
+
+            age.setText(ageSub.substring(2,4)+"-"+ageSub.substring(4,6)+"-"+ageSub.substring(6,8));
+
+            TextView tel  = convertview.findViewById(R.id.tell_text);
+            tel.setText(arItem.get(position).getTell());
+
+            TextView check  = convertview.findViewById(R.id.check_text);
+            check.setText("");
+
+
+
+            return convertview;
+        }
+    }
 
 }
