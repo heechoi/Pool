605ddde32fefafd58978bac3552143b05b8622df
heechoi <ekgml9904@naver.com>
2018-04-24 22:38:10 +0900

    barcode 하는 중

--- app/src/main/java/kr/or/dgit/bigdata/pool/BarCodeActivity.java
+++ app/src/main/java/kr/or/dgit/bigdata/pool/BarCodeActivity.java
@@ -9,27 +9,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
 
+import org.w3c.dom.Text;
+
 public class BarCodeActivity extends AppCompatActivity {
-    ImageView view;
+    private ImageView view;
+    private TextView code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_code);
+        setTitle("회원코드");
+        code = findViewById(R.id.code);
         MultiFormatWriter gen =  new MultiFormatWriter();
         SharedPreferences member = getSharedPreferences("member",MODE_PRIVATE);
+        int mno = member.getInt("mno",0);
 
-        String data = "1234560";
+        String data = mno+"";
         view = findViewById(R.id.barcode);
-
+        code.setText(mno+"");
         try{
-            final int WIDTH = view.getMaxWidth();
-            final int HEIGHT = view.getMaxHeight();
+            final int WIDTH = 300;
+            final int HEIGHT = 130;
 
             BitMatrix byteamap = gen.encode(data, BarcodeFormat.CODE_128,WIDTH,HEIGHT);
             Bitmap bitmap = Bitmap.createBitmap(WIDTH,HEIGHT,Bitmap.Config.ARGB_8888);
