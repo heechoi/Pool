6363880deb1465662a3ae59aeda0d4266cd6d6ed
HongBumHa <goldstate@naver.com>
2018-04-24 22:37:03 +0900

    예쁜 최다희님 잘 보시고 분석 바랍니다...

--- app/src/main/java/kr/or/dgit/bigdata/pool/fragment/ClassBoardRead.java
+++ app/src/main/java/kr/or/dgit/bigdata/pool/fragment/ClassBoardRead.java
@@ -1,19 +1,101 @@
 package kr.or.dgit.bigdata.pool.fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import kr.or.dgit.bigdata.pool.R;
 
 public class ClassBoardRead extends Fragment {
+    ImageView imgview;
+    Bitmap bmImg;
+    back tack;
+    Bitmap rotate;
+    String urlimg = "http://192.168.123.113:8080/pool/resources/upload/classboard/IMG8335610572756851618.jpg";
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.classboard_read, container, false);
+        imgview = root.findViewById(R.id.uplaod_img);
+        tack = new back();
+        tack.execute(urlimg);
         return root;
+    }
+    private class back extends AsyncTask<String, Integer, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+
+                URL myFileUrl = new URL(urls[0]);
+                HttpURLConnection conn = (HttpURLConnection) myFileUrl.openConnection();
+                conn.setDoInput(true);
+                conn.connect();
+                InputStream is = conn.getInputStream();
+
+                bmImg = BitmapFactory.decodeStream(is);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            rotate = rotateImage(bmImg);
+            return bmImg;
+        }
+
+        protected void onPostExecute(Bitmap img) {
+            imgview.setImageBitmap(rotate);
+        }
+    }
+
+    private Bitmap rotateImage(Bitmap bitmap) {
+        ExifInterface exifInterface = null;
+        Log.d("bum","===========시작");
+        File filePath = new File(bitmap.toString());
+        Log.d("bum","===========1");
+        try {
+            if (filePath != null) {
+                Log.d("bum","===========2");
+                exifInterface = new ExifInterface(filePath.getAbsolutePath());
+                Log.d("bum","===========3");
+            } else {
+              //  exifInterface = new ExifInterface(galleryPath);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+        Matrix m = new Matrix();
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                m.setRotate(90);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                m.setRotate(180);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                m.setRotate(270);
+                break;
+            default:
+                m.setRotate(0);
+        }
+        Bitmap rotated = Bitmap.createBitmap(bitmap, 0, 0, 300, 250, m, true);
+        return rotated;
     }
 }
