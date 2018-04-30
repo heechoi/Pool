7043b6d0b6b97418db2b6031422481abe4376baa
heechoi <ekgml9904@naver.com>
2018-05-01 01:08:03 +0900

    약간 수정

--- app/src/main/java/kr/or/dgit/bigdata/pool/fragment/TeacherLogin.java
+++ app/src/main/java/kr/or/dgit/bigdata/pool/fragment/TeacherLogin.java
@@ -56,7 +56,7 @@ public class TeacherLogin extends Fragment implements View.OnClickListener{
     private EditText id;
     private EditText pw;
     private Button loginBtn;
-    private String http ="http://192.168.0.239:8080/pool/restLogin/";
+    private String http ="http://211.107.115.62:8080/pool/restLogin/";
     private SharedPreferences tlogin;
     private SharedPreferences mlogin;
     private SharedPreferences state;
